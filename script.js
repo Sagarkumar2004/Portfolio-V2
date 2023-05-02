abd32557f3aae30cbedeec9ef39c3661f12d3669
Sagar kumar <sk8856330@gmail.com>
2023-05-02 14:42:56 +0530

    Add scrolling animation

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ document.querySelector(".menuBox").addEventListener("click", function(){
     
    
   });
+
+  
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+
+    } else{
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
